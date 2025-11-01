f5050df76c1831bc282bf8f895ab5f917c645f9a
kurmirajendra <rajendrakurmi78@gmail.com>
2025-11-01 23:38:38 +0530

    Queue1.c

--- Queue/Queue1.c
+++ Queue/Queue1.c
@@ -1,88 +1,81 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct Queue
+struct Student
 {
-    int rollno;
-    struct Queue *next;
-}*front,*rear;
+    char name[15];
+    struct Student *next;
+}*rear,*front;
 void add()
 {
-    struct Queue *ptr;
-    ptr=(struct Queue *)malloc(sizeof(struct Queue));
-    if(ptr==NULL)
-        printf("\nMemory not allocate ");
-    else
+    struct Student *st;
+    st=(struct Student *)malloc(sizeof(struct Student));
+    if(st==NULL)
     {
-        printf("\nEnetr Roll No. ");
-        scanf("%d",&ptr->rollno);
-        if(front==NULL)
-        {
-            ptr->next=NULL;
-            front=rear=ptr;
-        }
-        else
-        {
-          ptr->next=NULL;
-          rear->next=ptr;
-          rear=ptr;
-
-        }
+        printf("\n Memory not allocated ");
+        return;
     }
-}
-void display()
-{
+   else
+   {
+      printf("\nEnter Name ");
+      scanf("%s",&st->name);
     if(front==NULL)
     {
-        printf("\nNO Node Fround ");
+     st->next=NULL;
+     rear=front=st;
     }
     else
     {
-          struct Queue *ptr;
-       for(ptr=front;ptr!=NULL;ptr=ptr->next)
-        {
-            printf("\nRoll No. %d ",ptr->rollno);
-        }
-        printf("\n=======================\n ");
+        st->next=NULL;
+        rear->next=st;
+        rear=st;
     }
-
-
+   }
+}
+void show()
+{
+    if(front==NULL)
+        printf("\n No Student Found!! ");
+    else
+    {
+        struct Student *ptr;
+        for(ptr=front;(ptr);ptr=ptr->next)
+        {
+            printf("\n Student Name %s ",&ptr->name);
+            printf("\n--------------------- ");
+        }
+    }
 }
 void delNode()
 {
     if(front==NULL)
-        printf("\nNo Node Found ");
+        printf("\nQueue is UnderFlow ");
     else
     {
-         struct Queue *ptr;
-           ptr=front;
-           front=ptr->next;
-           free(ptr);
-           printf("\n Node %d deleted ",ptr->rollno);
+        struct Student *s;
+        s=front;
+        front=s->next;
+        free(s);
+        printf("\nStudent Deleted %s ",s->name);
+
     }
-
-
-
 }
 void main()
 {
- front=rear=NULL;
- int c;
- while(1)
- {
-     printf("\n1.ADD STUDENT ");
-     printf("\n2.DISPLAY ALL STUDENT ");
-     printf("\n3.DELETE STUDENT ");
-     printf("\n4.EXIT ");
-     printf("\nEnter YOUR Choice ");
-     scanf("%d",&c);
-     if(c==1)
-        add();
-    else if(c==2)
-        display();
-    else if(c==3)
-        delNode();
-     else
-        break;
- }
+    rear=front=NULL;
+    int ch;
+    do
+    {
+        printf("\n1.Enter Student (InQueue) ");
+        printf("\n2.Delete Student (DeQueue) ");
+        printf("\n3.Show All Students ");
+        printf("\n4.Exit");
+        printf("\nEnter your choice (1-4) ");
+        scanf("%d",&ch);
+        if(ch==1)
+            add();
+        else if(ch==2)
+          delNode();
+          else if(ch==3)
+            show();
+    }while(ch!=4);
 }
-
